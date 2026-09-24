d5bb1ce434d050224103c2eb12d6f4db289616dd
Saurabhpawar0199 <113376780+Saurabhpawar0199@users.noreply.github.com>
2026-09-24 18:16:13 +0530

    Update PageController.java

--- src/main/java/com/jsp/ojpms/controller/PageController.java
+++ src/main/java/com/jsp/ojpms/controller/PageController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+	@GetMapping("/")
+	public String root() {
+		return "home";
+	}
+
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
